15a7f9bab5fc356d768ef911d4d3b6a5f20b809e
Vince Forgetta <vince.forgetta@epi.jgh.mcgill.ca>
2012-08-23 15:25:31 -0400

    Fix bug where unable to parse chrom # from snp name.

--- bin/bimbam2annotate.bash
+++ bin/bimbam2annotate.bash
@@ -2,8 +2,14 @@
 
 FILES=($@)
 
-for MGFFILE in ${FILES[@]}
+for GENFILE in ${FILES[@]}
 do
-    BASE=$(basename ${MGFFILE} .mgf)
-    perl -p -e "s/(^[0-9]+)\-([0-9]+).*/\1-\2,\2,\1/g;" ${MGFFILE} > ${BASE}.ann
+    # This variant works when the chromosome number is in the file name e.g. chr22.gen
+    BASE=$(basename ${GENFILE} .gen)
+    CHROM=`echo ${GENFILE} | perl -p -e "s/.*chr([0-9XY]+).*/\1/;"`
+    awk "{ print \$2\",\"\$3\",\"${CHROM} }" ${GENFILE} > ${BASE}.ann
+    
+    # This variant work with MGF files, and only works when snp is named #-######, e.g. 9-1200000
+    # BASE=$(basename ${MGFFILE} .gen)
+    # perl -p -e "s/(^[0-9]+)\-([0-9]+).*/\1-\2,\2,\1/g;" ${MGFFILE} > ${BASE}.ann
 done
