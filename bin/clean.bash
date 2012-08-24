0b601cb7fe74b12ea36a2eb02f30729e1a1c72aa
Vince Forgetta <vince.forgetta@epi.jgh.mcgill.ca>
2012-08-24 15:36:46 -0400

    Info filter now optional

--- bin/clean.bash
+++ bin/clean.bash
@@ -3,15 +3,24 @@
 
 PREFIX=$1
 INFO_FILE=$2
-TMPDIR=$3
 
 GEMMA_RES="output/${PREFIX}.assoc.txt"
+BETA_LOWER=-1.5
+BETA_UPPER=1.5
+SE_LIMIT=0.01
+
 # Get header
-head -n 1 ${GEMMA_RES} > ${GEMMA_RES}.clean
-# Sort by SNP name
-tail -n +2 ${GEMMA_RES} | sort -k2,2 > ${GEMMA_RES}.sort
-# Filter for informativity >= 0.4
-join -1 2 -2 1 ${GEMMA_RES}.sort ${INFO_FILE} | awk '{ print $2, $1, $3, $4, $5, $6, $7, $8, $9, $10, $11 }' | sort -k2,2 >> ${GEMMA_RES}.clean
-# Delete tmp file(s)
-rm ${GEMMA_RES}.sort
+if [[ -z $INFO_FILE ]]; then
+    head -n 1 ${GEMMA_RES} > ${GEMMA_RES}.clean
+    tail -n +2 ${GEMMA_RES} | sort -k2,2 | \
+	awk "{ if ( (\$5 >= ${BETA_LOWER}) && (\$5 <= ${BETA_UPPER}) && (\$6 >= ${SE_LIMIT}) ) print \$0 }" \
+	>> ${GEMMA_RES}.clean
+else
+    head -n 1 ${GEMMA_RES} > ${GEMMA_RES}.clean
+    tail -n +2 ${GEMMA_RES} | sort -k2,2 | \
+	join -1 2 -2 1 - ${INFO_FILE} | \
+	awk '{ print $2, $1, $3, $4, $5, $6, $7, $8, $9, $10, $11 }' | \
+	awk "{ if ( (\$5 >= ${BETA_LOWER}) && (\$5 <= ${BETA_UPPER}) && (\$6 >= ${SE_LIMIT}) ) print \$0 }" \
+	>> ${GEMMA_RES}.clean
+fi
 
