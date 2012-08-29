edef9d798966cf42c288f4328c7326bb32f8073e
Vince Forgetta <vince.forgetta@epi.jgh.mcgill.ca>
2012-08-29 10:01:11 -0400

    Fix bug for parsing chrom number.

--- bin/bimbam2annotate.bash
+++ bin/bimbam2annotate.bash
@@ -1,12 +1,26 @@
 #!/bin/bash
 
 FILES=($@)
-
+REGEXP="[0-9]+"
 for GENFILE in ${FILES[@]}
 do
     # This variant works when the chromosome number is in the file name e.g. chr22.gen
     BASE=$(basename ${GENFILE} .gen)
-    CHROM=`echo ${GENFILE} | perl -p -e "s/.*chr([0-9XY]+).*/\1/;"`
+    CHROM=
+    # Try to parse chrom number from filename
+    if [[ ${BASE} =~ [0-9]+ ]]; then
+	CHROM=$BASE
+    else
+	CHROM=`echo ${GENFILE} | perl -p -e "s/.*chr([0-9]+).*/\1/;"`
+    fi
+    
+    # Test that parsing was suscessful
+    if [[ ! ${CHROM} =~ [0-9]+ ]]; then
+	echo "Could not parse chromosome number from filename: ${GENFILE}"
+	exit(1)
+    fi
+    
+    # if parsing was OK, generate annotation file
     awk "{ print \$2\",\"\$3\",\"${CHROM} }" ${GENFILE} > ${BASE}.ann
     
     # This variant work with MGF files, and only works when snp is named #-######, e.g. 9-1200000
