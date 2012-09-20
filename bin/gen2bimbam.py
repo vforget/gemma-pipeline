f040ded649451d1bdd70645324d9320a06d1b7a0
Vince Forgetta <vince.forgetta@epi.jgh.mcgill.ca>
2012-09-20 11:53:32 -0400

    Bug fix for calculating minor allele dosage.
    Bug: Calculates dosage of 2nd allele, assuming it is the minor allele. GEN format from IMPUTE does not always have minor allele as 2nd allele.

--- bin/gen2bimbam.py
+++ bin/gen2bimbam.py
@@ -8,17 +8,30 @@ import gc
 # Managable buffer size. For ~6,000 individuals consumes ~300MB of physical memory.
 buffer_size = 10800000
 
-def to_float(m):
-    ''' Calculates dosage values for minor allele '''
-    return [[f[1], f[4], f[3]] + ["%0.3f" % (float(f[i+1]) + float(f[i+2])*2) for i in range(5, len(f), 3)] for f in m]
+def gen_to_mgf(f):
+    ''' Calculates dosage of minor allele for a all individuals for one SNP'''
+    # Dosage list for 2nd allele
+    a2 = [(float(f[i+1]) + float(f[i+2])*2) for i in range(5, len(f), 3)]
+    a2d = sum(a2)
+    totd = ((len(f)-5) / 3)
+    # if sum dosage for 2nd allele exceed minor allele threshold exceeds 
+    # 1/2 the total possible dosage for all alleles, it is not the 
+    # minor allele. Return the dosage for the other allele.
+    if a2d > totd:
+        sys.stderr.write("Warning: SNP %s -- Dosage for allele 2 exceeds MAF threshold \
+of 0.5: %s > %s. Returning dosage for allele 1 instead.\n" % (f[1], a2d, totd))
+        return [f[3], f[4]] + ["%0.3f" % (float(f[i+1]) + float(f[i])*2) \
+                                   for i in range(5, len(f), 3)]
+    return [f[4], f[3]] + ["%0.3f" % x for x in a2]
 
-def dosage(a):
+def write_mgf(a):
     ''' Gets dosage values for a chunk of data. 
     Saves dosage values to pre-determined a filename. '''
     d = a[0]
     fn = a[1]
     m = [x.strip().split(" ") for x in d]
-    t = to_float(m)
+    t = [[f[1]] + gen_to_mgf(f) for f in m]
+    sys.stderr.write("Writing output to %s\n" % (fn))
     o = open(fn, "w")
     print >> o, "\n".join(",".join(r) for r in t)
     o.close()
@@ -50,7 +63,7 @@ if __name__ == "__main__":
         if len(chunk) == POOL_SIZE:
             p = Pool(POOL_SIZE)
             # Calculate dosage for chunks in parallel
-            p.map(dosage, chunk)
+            p.map(write_mgf, chunk)
             chunk = [] 
             p.close()
             # Clean up, prevent small but appreciable increase in memory usage.
@@ -59,6 +72,6 @@ if __name__ == "__main__":
         c += 1
     # Process last chunks of data
     p = Pool(POOL_SIZE)
-    p.map(dosage, chunk)
+    p.map(write_mgf, chunk)
     p.close()
     gfh.close()
