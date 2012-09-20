bc0fa9bb82fd2927d0182e9e01ce07534beeb3e8
Vince Forgetta <vince.forgetta@epi.jgh.mcgill.ca>
2012-09-20 11:53:07 -0400

    Added queue all.q

--- bin/gen2bimbam_batch.bash
+++ bin/gen2bimbam_batch.bash
@@ -8,5 +8,5 @@ TMPDIR=~/tempdata
 mkdir sge_log
 for GENFILE in ${FILES[@]}
 do
-    echo "${BINDIR}/gen2bimbam.bash ${GENFILE} ${TMPDIR} ${POOL_SIZE}" | qsub -V -cwd -pe grinux_pe ${POOL_SIZE} -N g2b${PREFIX} -o sge_log -e sge_log
+    echo "${BINDIR}/gen2bimbam.bash ${GENFILE} ${TMPDIR} ${POOL_SIZE}" | qsub -V -cwd -pe grinux_pe ${POOL_SIZE} -N g2b${PREFIX} -o sge_log -e sge_log -q all.q
 done
