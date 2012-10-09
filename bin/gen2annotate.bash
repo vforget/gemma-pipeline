d1290e1c1c25ffe55b4983ff0a9ced1667a3180f
Vince Forgetta <vince.forgetta@epi.jgh.mcgill.ca>
2012-10-09 12:12:50 -0400

    Renamed bimbam2annotate.bash

--- bin/gen2annotate.bash
+++ bin/gen2annotate.bash
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+# Script only works when the chromosome number is in the file name e.g. chr22.gen or 22.gen
+
 FILES=($@)
 REGEXP="[0-9]+"
 for GENFILE in ${FILES[@]}
 do
-    # This variant works when the chromosome number is in the file name e.g. chr22.gen
     BASE=$(basename ${GENFILE} .gen)
     CHROM=
     # Try to parse chrom number from filename
@@ -23,7 +24,4 @@ do
     # if parsing was OK, generate annotation file
     awk "{ print \$2\",\"\$3\",${CHROM}\" }" ${GENFILE} > ${BASE}.ann
     
-    # This variant work with MGF files, and only works when snp is named #-######, e.g. 9-1200000
-    # BASE=$(basename ${MGFFILE} .gen)
-    # perl -p -e "s/(^[0-9]+)\-([0-9]+).*/\1-\2,\2,\1/g;" ${MGFFILE} > ${BASE}.ann
 done
