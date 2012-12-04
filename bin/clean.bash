6c839aa6d0ce0947224d432207c92490c7a42fb7
Vince Forgetta <vince.forgetta@epi.jgh.mcgill.ca>
2012-12-04 15:14:23 -0500

    Updated clean.bash
    Fixed bug for filtering from SNP file.ddd

--- bin/clean.bash
+++ bin/clean.bash
@@ -18,7 +18,7 @@ fi
 if [[ -n $INFO_FILE ]]; then
     head -n 1 ${GEMMA_RES} > ${GEMMA_RES}.clean
     tail -n +2 ${GEMMA_RES} | sort -k2,2 | \
-	join -1 1 -2 1 - ${INFO_FILE} | \
+	join -1 2 -2 1 - ${INFO_FILE} | \
 	awk '{ print $2, $1, $3, $4, $5, $6, $7, $8, $9, $10, $11 }' | \
 	awk "{ if ( (\$5 >= ${BETA_LOWER}) && (\$5 <= ${BETA_UPPER}) && (\$6 >= ${SE_LIMIT}) ) print \$0 }" \
 	>> ${GEMMA_RES}.clean
@@ -30,4 +30,4 @@ else
 	>> ${GEMMA_RES}.clean  
 fi
 
-rm ${GEMMA_RES}
+# rm ${GEMMA_RES}
